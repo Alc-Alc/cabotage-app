dd514b2410868ffd4bdfdd405bd91d721ac60891
Alc-Alc <alc@localhost>
2026-08-25 21:14:11 +0530

    chore(_types): remove `require_not_none`
    can add this later if it's required

--- cabotage/_types/__init__.py
+++ cabotage/_types/__init__.py
@@ -1,14 +1,5 @@
 from typing import cast
 
 
-class InvariantViolationError(Exception): ...
-
-
 def assume_not_none[T](val: T | None, /, *, because: str) -> T:
     return cast("T", val)
-
-
-def require_not_none[T](val: T | None, /, *, because: str) -> T:
-    if val is None:
-        raise InvariantViolationError(f"Invariant violated; assumption: {because}")
-    return val
